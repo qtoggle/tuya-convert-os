e6ee2c802f1940cc1a2ac5f692ddeaccd53fb121
Calin Crisan <ccrisan@gmail.com>
2020-03-01 14:11:08 +0200

    Start tcfrontend service after network

--- setup.sh
+++ setup.sh
@@ -10,9 +10,6 @@ cd /root
 rngd -r /dev/urandom
 echo 1 > /proc/sys/kernel/panic
 
-echo " * changing hostname"
-sed -i 's/raspberrypi/tuya-convert/' /etc/hostname /etc/hosts
-
 echo " * downloading tuya-convert"
 curl -L https://github.com/ct-Open-Source/tuya-convert/archive/31856ada24a1e951b4da849bff2ed056f544c51b.tar.gz -o tuya-convert.tar.gz
 tar xvf tuya-convert.tar.gz
@@ -58,6 +55,8 @@ echo " * creating tcfrontend service"
 cat > /lib/systemd/system/tcfrontend.service << EOF
 [Unit]
 Description=Tuya-Convert Frontend
+Wants=network-online.target
+After=network-online.target
 
 [Service]
 Type=simple
@@ -83,6 +82,9 @@ echo "denyinterfaces ap0" >> /etc/dhcpcd.conf
 
 echo " * removing setup"
 rm /setup.sh
+
+echo " * changing hostname"
+sed -i 's/raspberrypi/tuya-convert/' /etc/hostname /etc/hosts
 
 echo " * setup done"
 sync
