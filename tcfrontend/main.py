870e29a11adb8b7870a857674bb3c490ecddac65
Calin Crisan <ccrisan@gmail.com>
2020-03-01 13:00:24 +0200

    tcfrontend: listen on eth0 and wlan0

--- tcfrontend/main.py
+++ tcfrontend/main.py
@@ -1,16 +1,35 @@
 
 import asyncio
+import fcntl
 import logging
+import socket
+import struct
+
+from tornado import httpserver
 
 from tcfrontend import webserver
 from tcfrontend import states
 
+
+IFNAMES = ['eth0', 'wlan0']
+PORT = 80
 
 logger = None
 
 
 async def init():
     states.init()
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(
+        fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15].encode())
+        )[20:24]
+    )
 
 
 def main() -> None:
@@ -28,7 +47,16 @@ def main() -> None:
     logger.info('hello!')
 
     app = webserver.make_app()
-    app.listen(80)
+
+    for ifname in IFNAMES:
+        try:
+            address = get_ip_address(ifname)
+
+        except IOError:
+            continue
+
+        server = httpserver.HTTPServer(app)
+        server.listen(PORT, address=address)
 
     asyncio.get_event_loop().create_task(init())
     asyncio.get_event_loop().run_forever()
